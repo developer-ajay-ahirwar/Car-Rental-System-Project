8c9088cacba86357c1e13d54224c2d9f287483c4
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2024-06-07 21:01:31 +0530

    full all Customer class version 0.6
    full all Customer class version 0.6

--- Customer.java
+++ Customer.java
@@ -1,3 +1,18 @@
-public class Customer {
+class Customer {
+    private String customerName;
+    private String customerID;
+
+    public Customer(String customerName,String customerID){
+        this.customerName = customerName;
+        this.customerID = customerID;
+    }
+
+    public String getCustomerName(){
+        return this.customerName;
+
+    }
+    public String getCustomerID(){
+        return this.customerID;
+    }
 
 }
