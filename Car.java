4a7da6ab2f787df2ab3e2f827f90ef1a4d4310c4
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2024-06-07 20:39:25 +0530

    Create a Constractor and getter or setter
    Create a Constractor and getter or setter
    version 0.3

--- Car.java
+++ Car.java
@@ -4,4 +4,33 @@ class Car {
   private String model;
   private double basePriceOfDay;
   private boolean isAvailable;
+
+  public Car(String name,String brand, String model , double basePriceOfDay , boolean isAvailable){
+    this.name = name;
+    this.brand = brand;
+    this.model = model;
+    this.basePriceOfDay = basePriceOfDay;
+    this.isAvailable = isAvailable;
+  }
+  public String getName(){
+    return this.name;
+  }
+  public String getBrand(){
+    return this.brand;
+  }
+  public String getModel(){
+    return this.model;
+  }
+  public double  getBasePriceOfDay(){
+    return this.basePriceOfDay;
+  }
+  public boolean getIsAvailable(){
+    return this.isAvailable;
+  }
+  public void setBasePriceOfDay(double basePriceOfDay){
+    this.basePriceOfDay = basePriceOfDay;
+  }
+  public void setIsAvailable(boolean IsAvailable){
+    this.isAvailable = IsAvailable;
+  }
 }
