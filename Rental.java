f49ab6762c36f871d8901dc6486be5c06ce6a365
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2024-06-07 21:18:23 +0530

    full all Rental class version 0.8
    full all Rental class version 0.8

--- Rental.java
+++ Rental.java
@@ -1,3 +1,21 @@
-public class Rental {
+class Rental {
+    private Car car;
+    private Customer customer;
+    private int days;
 
+    public Rental(Car car , Customer customer , int days){
+        this.car = car;
+        this.customer = customer;
+        this.days = days;
+    }
+
+    public Car getCar(){
+        return this.car;
+    }
+    public Customer getCustomer(){
+        return customer;
+    }
+    public int getDays(){
+        return this.days;
+    }
 }
