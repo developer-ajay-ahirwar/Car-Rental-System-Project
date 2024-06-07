241c129d29a2ea505aab52400bd9d46eb9de5a1c
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2024-06-07 23:17:37 +0530

    change car name to car ID version 12
    change car name to car ID version 12

--- Car.java
+++ Car.java
@@ -1,20 +1,20 @@
 class Car {
-  private String name;
+  private String ID;
   private String brand;
   private String model;
   private double basePriceOfDay;
   private boolean isAvailable;
   //remove the Argument is isAvailable in the Constractor
-  public Car(String name,String brand, String model , double basePriceOfDay){
-    this.name = name;
+  public Car(String ID,String brand, String model , double basePriceOfDay){
+    this.ID = ID;
     this.brand = brand;
     this.model = model;
     this.basePriceOfDay = basePriceOfDay;
     //default set the true isAvailable
     this.isAvailable = true;
   }
-  public String getName(){
-    return this.name;
+  public String getID(){
+    return this.ID;
   }
   public String getBrand(){
     return this.brand;
