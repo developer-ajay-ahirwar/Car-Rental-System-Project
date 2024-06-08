f9a273980816420cdeaf255701c480f0ca48c98e
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2024-06-08 22:56:14 +0530

    SetUp the Main Class version 14
    SetUp the Main Class version 14

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Car car1 = new Car("C001", "Toyta", "Tabera", 150);
+        Car car2 = new Car("C002", "Maroti", "SUV", 180);
+        Car car3 = new Car("C003", "TATA", "Thar", 200);
+        Car car4 = new Car("C004", "swift", "farari", 250);
+        CarRentalSystem carRentalSystem = new CarRentalSystem();
+        carRentalSystem.addCar(car1);
+        carRentalSystem.addCar(car2);
+        carRentalSystem.addCar(car3);
+        carRentalSystem.addCar(car4);
+
+        carRentalSystem.main();
     }
 }
